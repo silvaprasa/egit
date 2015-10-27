407298f91a790bfc8259321e7d8b59de22c604f9
Prasad <silvaprasa@gmail.com>
2015-10-27 19:21:15 +0530

    change salesdata display method

--- EGit/src/com/prasad/egit/SalesData.java
+++ EGit/src/com/prasad/egit/SalesData.java
@@ -1,9 +1,11 @@
 package com.prasad.egit;
 
 public class SalesData {
-   int data[] = {0,4,42};
-   
-   public void display(){
-	   System.out.println("TODO : ");
-   }
+	int data[] = { 0, 4, 42 };
+
+	public void display() {
+		for (int i : data) {
+			System.out.println("" + i);
+		}
+	}
 }
