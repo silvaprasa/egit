52b39ae87d429c96dcc35c220e937d10b8736916
Prasad <silvaprasa@gmail.com>
2015-10-27 16:28:08 +0530

    change output big letters.

--- EGit/src/com/prasad/egit/Main.java
+++ EGit/src/com/prasad/egit/Main.java
@@ -4,16 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		SalesData data = new SalesData();
-		
+
 		displayGreeting();
 		data.display();
-		
+
 		System.out.println("test 1");
-		
+
 	}
-	
-	private static void displayGreeting(){
-		System.out.println("Hello Happy Sales People");
-		System.out.println("This App Shows sales data");
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE.");
+		System.out.println("THIS APP SHOWS SALES DATA.");
+		System.out.println("----------------------------");
 	}
 }
